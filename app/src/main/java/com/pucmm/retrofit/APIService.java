3236ded99e8c40830a47904a203fb4138d5b60a9
unknown <f.ant.pena@gmail.com>
2020-11-25 22:38:01 -0400

    init commit

---
+++ app/src/main/java/com/pucmm/retrofit/APIService.java
@@ -0,0 +1,17 @@
+package com.pucmm.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
+public interface APIService {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+    @GET("posts/{postId}/comments")
+    Call<List<Comment>> getCommentsByPost(@Path("postId") int postId);
+
+}
